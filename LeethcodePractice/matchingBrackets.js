0bdedeb826e409c66eee40f72bef1570e34e723c
Work Goonja <workgoonja@goonjas-MacBook-Pro.local>
2025-04-27 21:54:44 -0400

    corrected some parts

--- LeethcodePractice/matchingBrackets.js
+++ LeethcodePractice/matchingBrackets.js
@@ -2,26 +2,39 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    let isMaching = new Map().set("{", "}").set("[", "]").set("(", ")");
+let counting = 1;
+var isValid = function (s) {
+    counting++;
     let stack = [];
-    s.split("").forEach(a => {
-        if(isMaching.has(a)) {
-            stack.push(a);
+    const isClosing = new Set().add('}').add("]").add(")");
+    const isMaching = new Map().set("{", "}").set("(" , ")").set("[", "]");
+
+    for(let i = 0; i < s.length; i++) {
+        if((stack.length === 0) && (isClosing.has(s[i]))) return false;
+        else if(isMaching.has(s[i])) {
+            stack.push(s[i]);
         } else {
-            //Get the top element of stack
-            let top = stack[stack.length-1];
-            if(isMaching.get(top) === a) {
-                stack.pop()
-            }
+             const top = stack.pop();
+             if(isMaching.get(top) !== s[i]) return false;
         }
-    })
+    }
 
-    if(stack.length === 0) return true 
-    else return false
+    if(stack.length === 0) return true;
+    else return false;
+
+
 };
-
-console.log(isValid("()[]{}")); // true
-console.log(isValid("(}")); // false
-console.log(isValid("([])")); //true
-console.log(isValid("([]){")); // false
+console.log(counting);
+console.log(isValid("()[]{}")); // 1 true - correct
+console.log(counting);
+console.log(isValid("(}")); // 2 false - correct
+console.log(counting);
+console.log(isValid("([])")); //3 true - correct
+console.log(counting);
+console.log(isValid("([]){")); //4 false - correct
+console.log(counting);
+console.log(isValid("}()[]")); //5 false - corrcect
+console.log(counting); 
+console.log(isValid(")(){}")); //6 false - correct 
+console.log(counting);
+console.log(isValid("]")); //7 false = correct
